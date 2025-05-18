e4981ef5d65cabe7d4e4f92922f671772b10259e
SachindaBHerath <it24102250@my.sliit.lk>
2025-05-18 23:37:33 +0530

    Implement CRUD Operation

---
+++ src/main/java/com/sliit/autocarepro/Controller/ServiceRecordController.java
@@ -0,0 +1,34 @@
+package com.sliit.autocarepro.Controller;
+
+import com.sliit.autocarepro.Model.ServiceRecord;
+import com.sliit.autocarepro.Service.SRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+public class ServiceRecordController {
+    @Autowired
+    private SRecordService sRecordService;
+
+    @PostMapping
+    public String registerServiceRecord(@RequestBody ServiceRecord serviceRecord) {
+        return sRecordService.registerServiceRecord(serviceRecord);
+    }
+
+    @GetMapping
+    public List<ServiceRecord> getAllServiveRecord() {
+        return sRecordService.getAllServiceRecord();
+    }
+
+    @PutMapping
+    public String updateServiceRecord(@PathVariable("recordID")int recordID,@RequestBody ServiceRecord serviceRecord) {
+
+        return sRecordService.updateServiceRecord(serviceRecord,recordID);
+    }
+
+    @DeleteMapping(params = "id")
+    public void deleteServiceRecord(@RequestParam int id) {
+        sRecordService.deleteServiceRecord(id);
+    }
+}
---
+++ src/main/java/com/sliit/autocarepro/Repository/ServiceRecordRepository.java
@@ -0,0 +1,45 @@
+package com.sliit.autocarepro.Repository;
+
+import com.sliit.autocarepro.Model.ServiceRecord;
+import org.springframework.stereotype.Repository;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Repository
+public class ServiceRecordRepository {
+    private LinkedList<ServiceRecord> serviceRecords = new LinkedList<>();
+    private static int nextrecordId = 1;
+
+    public String save(ServiceRecord serviceRecord) {
+        if (serviceRecord.getRecordID() == 0) {
+            serviceRecord.setRecordID(nextrecordId++);
+        }
+        serviceRecords.add(serviceRecord);
+        return "Registration Successful";
+    }
+
+    public List<ServiceRecord> findAll() {
+        return new LinkedList<>(serviceRecords);
+    }
+
+    public ServiceRecord findById(int id) {
+        return serviceRecords.stream().filter(c -> c.getRecordID() == id).findFirst().orElse(null);
+    }
+
+    public void deleteByRecordId(int recordID) {
+        serviceRecords.removeIf(c -> c.getRecordID() == recordID);
+    }
+
+    public String update(ServiceRecord serviceRecord, int recordID) {
+        ServiceRecord existing = findById(recordID);
+        if (existing != null) {
+            existing.setDate(serviceRecord.getDate());
+            existing.setCustomer(serviceRecord.getCustomer());
+            existing.setVehicle(serviceRecord.getVehicle());
+            existing.setService(serviceRecord.getService());
+        }
+        return "Update Successful";
+
+    }
+}
