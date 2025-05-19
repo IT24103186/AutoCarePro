c5d6f8ec6ded33d2691063c1a2f2eb6d37d3a46f
Binuwara <it24102647@my.sliit.lk>
2025-05-19 15:42:12 +0530

    add Log directory

--- src/main/java/com/sliit/autocarepro/Repository/VehicleRepository.java
+++ src/main/java/com/sliit/autocarepro/Repository/VehicleRepository.java
@@ -1,7 +1,13 @@
 package com.sliit.autocarepro.Repository;
 
+import com.sliit.autocarepro.Model.ServiceRecord;
 import com.sliit.autocarepro.Model.Vehicle;
 import org.springframework.stereotype.Repository;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,7 +21,9 @@ public class VehicleRepository {
             vehicle.setVehicleId(nextId++);
         }
         Vehicles.add(vehicle);
+        saveToTextFile();
         return "Registration Successful";
+
     }
 
     public List<Vehicle> findAll() {
@@ -37,9 +45,42 @@ public class VehicleRepository {
             existing.setPlateNumber(vehicle.getPlateNumber());
             existing.setVIN(vehicle.getVIN());
         }
+        saveToTextFile();
     }
 
     public void delete(int vehicleId) {
         Vehicles.removeIf(c -> c.getVehicleId() == vehicleId);
+        saveToTextFile();
+    }
+
+    private String saveToTextFile() {
+        String folderPath = "src/main/java/com/sliit/autocarepro/Log";
+        String filePath = folderPath + File.separator + "vehicleSaveLog.txt";
+
+        // Create the Log folder if it doesn't exist
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            boolean folderCreated = folder.mkdirs();
+            if (!folderCreated) {
+                return "Error creating Log folder at " + folderPath;
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Vehicle record : Vehicles) {
+                writer.write(String.format("vehicleID: %d, make: %s, model: %s, color: %s, year: %s, plateNumber: %s, vin: %s",
+                        record.getVehicleId(),
+                        record.getMake(),
+                        record.getModel(),
+                        record.getColor(),
+                        record.getYear(),
+                        record.getPlateNumber(),
+                        record.getVIN()));
+            }
+            return "Data successfully saved to " + filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error saving data to file: " + e.getMessage();
+        }
     }
 }
