ccacaf02458d06bfad7158ea8778a7e7d5038f32
Binuwara <it24102647@my.sliit.lk>
2025-05-20 07:57:58 +0530

    add interface for user class

--- src/main/java/com/sliit/autocarepro/Service/UserService.java
+++ src/main/java/com/sliit/autocarepro/Service/UserService.java
@@ -1,37 +1,13 @@
 package com.sliit.autocarepro.Service;
 
 import com.sliit.autocarepro.Model.User;
-import com.sliit.autocarepro.Repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
-public class UserService {
 
+public interface UserService {
 
-
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;}
-
-
-    public String registeruser(User user) {
-        return userRepository.save(user);
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public void updateUser(User user, int id) {
-        userRepository.update(user,id);
-    }
-
-    public void deleteUser(int id) {
-        userRepository.delete(id);
-    }
+    public String registerUser(User user);
+    public List<User> getAllUsers();
+    public void updateUser(User user, int id);
+    public void deleteUser(int id);
 }
---
+++ src/main/java/com/sliit/autocarepro/Service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.sliit.autocarepro.Service;
+
+import com.sliit.autocarepro.Model.User;
+import com.sliit.autocarepro.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;}
+
+    public String registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void updateUser(User user, int id) {
+        userRepository.update(user,id);
+    }
+
+    public void deleteUser(int id) {
+        userRepository.delete(id);
+    }
+}
